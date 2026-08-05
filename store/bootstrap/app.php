d9ef5955fcf05cc06c4fed4d89730345fb850947
John Mark B. Pagaran <pagaranjohnmark@minsu.edu.ph>
2026-08-06 00:28:23 +0800

    Fix Mixed Content error - Force HTTPS for assets in production
    - Added URL::forceScheme('https') in AppServiceProvider
    - Added trustProxies configuration in bootstrap/app.php
    - Trust all Railway proxies with X-Forwarded headers
    - Added ASSET_URL to .env.example
    - This fixes HTTP assets loading on HTTPS site

--- store/bootstrap/app.php
+++ store/bootstrap/app.php
@@ -17,6 +17,12 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
 
+        // Trust Railway proxies for HTTPS
+        $middleware->trustProxies(at: '*', headers: \Illuminate\Http\Request::HEADER_X_FORWARDED_FOR | 
+            \Illuminate\Http\Request::HEADER_X_FORWARDED_HOST | 
+            \Illuminate\Http\Request::HEADER_X_FORWARDED_PORT | 
+            \Illuminate\Http\Request::HEADER_X_FORWARDED_PROTO);
+
         $middleware->web(append: [
             HandleAppearance::class,
             HandleInertiaRequests::class,
